231a91f253fdbf2ca6a6f172ed9a65c95c9b90a6
Thomas <hollandpirates@gmail.com>
2019-03-06 19:38:05 +0100

    Comment backward search bash script

--- terminal/backward_search.sh
+++ terminal/backward_search.sh
@@ -1,28 +1,40 @@
 #!/usr/bin/env bash
 
 # By running this script, you can see all the SyncSource signals emitted. and the parameters
+# Instructions: run this script, open a pdf in Evince which was compiled with synctex and ctrl+click somewhere. You should see a message appearing in stdout with the SyncSource signal which was emitted
 
 main()
 {
+    # Create a temporary file which will hold the output of dbus-monitor
     output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
     echo "Output file: "
     echo ${output}
-    server &> ${output} &
+
+    # Start the monitor process (defined below) in the background, redirect output to the temporary file
+    monitor &> ${output} &
+
     echo "Waiting:"
+
     while [[ true ]]; do
+        # grep the output for the SyncSource signal
         until grep -q SyncSource ${output}
         do
-            :
+            : # no-op
         done
         echo
         echo "SyncSource signal received, content:"
+
+        # Print the next lines
         grep --text -A 6 SyncSource ${output}
-        # Empty output
+
+        # Empty temp file
         truncate -s 0 ${output}
     done
 }
 
-server()
+# Copy the output of dbus-monitor
+# Note that it is possible to monitor signals of only one object path, but then you cannot view the content of the signal
+monitor()
 {
     dbus-monitor |
     while read -r line; do
@@ -30,4 +42,4 @@ server()
     done
 }
 
-main "$@"
+main
